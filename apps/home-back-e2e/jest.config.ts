1c0434c3772d7718223a956ba5a1a031ba0b5fd9
Yurly123 <joo7won@gmail.com>
2023-02-26 10:46:40 +0900

    Add apps

---
+++ apps/home-back-e2e/jest.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default {
+  displayName: 'home-back-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/home-back-e2e',
+};
