8c0acc2bc94c0497adc8dd8e293afae547c80243
Yurly123 <joo7won@gmail.com>
2023-02-26 15:21:25 +0900

    Add navigation component

--- apps/home/web/src/app/app.component.ts
+++ apps/home/web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-site-root',
+  selector: 'my-site-home-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
