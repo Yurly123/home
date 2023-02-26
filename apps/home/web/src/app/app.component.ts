fe4836af0fd849b6725a4749a5f1102fc1940f41
Yurly123 <joo7won@gmail.com>
2023-02-26 14:19:45 +0900

    Change project directories

---
+++ apps/home/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-site-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'home-web';
+}
